14e3f450a74f636a89033e6955593c0c1ff3ceb6
Robin-Barwari <70271799+Robin-Barwari@users.noreply.github.com>
2021-08-30 13:17:48 +0200

    Xaml

---
+++ WpfMeddelande/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMeddelande
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Skriv ut en varning
+            rutaMeddelande.Text = "Servern är nere!";
+        }
+
+        private void Click_OK(object sender, RoutedEventArgs e)
+        {
+            // Deklarera en sträng
+            string texten = "";
+            texten = rutaText.Text;
+
+            // Läs in texten
+            Console.WriteLine(texten);
+
+            // Skriv in i rutan
+
+        }
+    }
+}
