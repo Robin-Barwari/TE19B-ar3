05ce5a77acd13cc3ce4cb781f54e083e2a5f7253
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2021-10-25 13:11:24 +0200

    Inkomster + Labb 9

---
+++ Objekt/Inkomster/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inkomster
+{
+    class PersonInkomst
+    {
+        public string Namn = "";
+        public int Lön = 0;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program för att registrera löner");
+
+            List<PersonInkomst> lista = new List<PersonInkomst>();
+
+            // Upprepa tills användaren skriver tomt
+            while (true)
+            {
+                // Lagra uppgifter i ett objekt
+                PersonInkomst objekt = new PersonInkomst();
+                
+                // Ange namn och lön
+                Console.WriteLine("Ange ditt namn: (Enter avbryter)");
+                objekt.Namn = Console.ReadLine();
+
+                // Avbryt om namn är tomt
+                if (objekt.Namn == "")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Ange din månadslön: ");
+                objekt.Lön = ReadInt();
+
+                // Lägg in i listan
+                lista.Add(objekt);
+            }
+
+
+        }
+        static int ReadInt()
+        {
+            int månadslön;
+            while (!int.TryParse(Console.ReadLine(), out månadslön))
+            {
+                Console.WriteLine("Ange bara heltal. Försök igen.");
+            }
+            return månadslön;
+        }
+    }
+}
---
+++ Objekt/Labb 9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inkomster
+{
+    class PersonInkomst
+    {
+        public string Förnamn = "";
+        public string Efternamn = "";
+        public string NamnAdress = "";
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program för att registrera löner");
+            List<PersonInkomst> lista = new List<PersonInkomst>();
+
+            // Upprepa tills användaren skriver tomt
+            while (true)
+            {
+                // Lagra uppgifter i ett objekt
+                PersonInkomst objekt = new PersonInkomst();
+                
+                // Ange namn och lön
+                Console.WriteLine("Ange ditt förnamn: (Enter avbryter)");
+                objekt.Förnamn = Console.ReadLine();
+
+                // Avbryt om namn är tomt
+                if (objekt.Förnamn == "")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Ange din Efternamn: ");
+                objekt.Efternamn = Console.ReadLine();
+
+                Console.WriteLine("Ange gatuadress: ");
+                objekt.NamnAdress = Console.ReadLine();
+                
+
+                // Lägg in i listan
+                lista.Add(objekt);
+            }
+        }
+    }
+}
