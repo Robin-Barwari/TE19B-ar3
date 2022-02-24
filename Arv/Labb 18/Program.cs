5818572cc3073b67462e99d7cd70b56cd7d615aa
Robin-Barwari <robin.barwari@elev.ga.ntig.se>
2022-02-24 10:01:18 +0100

    labb 18

---
+++ Arv/Labb 18/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Labb_18
+{
+    public class Creature
+    {
+        public int Health { get; set; }
+        public int Shield { get; set; }
+        protected string Name { get; set; }
+
+        public Creature(string name)
+        {
+            Name = name;
+        }
+
+        public string GetInfo()
+        {
+            return "";
+        }
+    }
+    public class Knight : Creature
+    {
+        protected string Weapon { get; set; }
+        public Knight(string name, string weapon) : base(name)
+        {
+            Weapon = weapon;
+        }
+    }
+
+    public class Dragon : Creature
+    {
+        protected string Color { get; set; }
+        public Dragon(string name, string color) : base(name)
+        {
+            Color = color;
+        }
+
+        public void Attack()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
